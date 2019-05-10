a7a80821ca40abe644148785fb2a83b378bf9063
Harshit Gupta <kohthecodemaster@gmail.com>
2019-05-10 13:41:42 +0530

    Init Commit. [Layout - GamePlayActivity]
    /*
     *  Time Stamp: 10th May 2K19, 01:32 PM..!!
     *
     *  Project Name    :   Seven Up Seven Down
     *  Status          :   Work In Progress
     *
     *  Change Log:
     *
     *  Init Commit:
     *  1. Designed the layout for GamePlayActivity
     *
     *  Code Developed By,
     *  ~K.O.H..!! ^__^
     */
    
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

---
+++ app/src/main/java/dev/koh/sevenupsevendown/GamePlayActivity.java
@@ -0,0 +1,28 @@
+package dev.koh.sevenupsevendown;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
+
+/*
+ *  Time Stamp: 10th May 2K19, 01:32 PM..!!
+ *
+ *  Project Name    :   Seven Up Seven Down
+ *  Status          :   Work In Progress
+ *
+ *  Change Log:
+ *
+ *  Init Commit:
+ *  1. Designed the layout for GamePlayActivity
+ *
+ *  Code Developed By,
+ *  ~K.O.H..!! ^__^
+ */
